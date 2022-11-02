9f8f88522b1385d021c29f04aa0c306d23ee519b
Alessandro Santini <alessandro.santini@toremo.com>
2022-11-02 14:06:23 +0100

    Added a hello world endpoint

--- owner/cmd/owner.go
+++ owner/cmd/owner.go
@@ -1,8 +1,18 @@
 package main
 
-import "github.com/labstack/echo/v4"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
 	app := echo.New()
+	app.GET("/", helloWorld)
 	app.Start("localhost:3000")
+}
+
+func helloWorld(c echo.Context) error {
+	c.String(http.StatusOK, "Hello Peter!")
+	return nil
 }
