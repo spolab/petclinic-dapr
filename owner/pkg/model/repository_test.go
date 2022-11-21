9813dc722d8d7f55687ea4ac2ff0d7b6dad825f9
Alessandro Santini <alessandro.santini@fiserv.com>
2022-11-21 10:23:56 +0100

    Added ability to run tests locally

--- owner/pkg/model/repository_test.go
+++ owner/pkg/model/repository_test.go
@@ -1,1 +1,24 @@
 package model_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/dapr/go-sdk/client"
+	"github.com/spolab/petclinic/owner/pkg/model"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// Tests the save of an aggregate root with one event
+func TestSaveHappyPath(t *testing.T) {
+	c, err := client.NewClient()
+	require.Nil(t, err)
+	repo, err := model.NewRepository(c, "owner-state", "owner-pubsub", "owner")
+	require.Nil(t, err)
+	require.NotNil(t, repo)
+	aggregate := model.Owner{
+		Id: "id",
+	}
+	assert.Nil(t, repo.Save(context.TODO(), &aggregate))
+}
